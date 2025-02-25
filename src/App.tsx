b20f34f8fe835a9bd8898347c89aff5c97d7bf83
LockHowl <omid3600@yahoo.com>
2025-02-25 18:50:11 -0500

    Initial commit - Setup React TS Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-2xl font-bold">Anime Schedule Clone</h1>
+    </div>
+  )
+}
+
+export default App
